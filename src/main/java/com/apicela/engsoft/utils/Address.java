5483365a58daf7682fa77227bf609dd37506f79f
apicela <trab.jamilsouza@gmail.com>
2024-09-06 17:43:12 -0300

    addres update

--- src/main/java/com/apicela/engsoft/utils/Address.java
+++ src/main/java/com/apicela/engsoft/utils/Address.java
@@ -7,4 +7,10 @@ import lombok.Data;
 @Embeddable
 public class Address {
     private String street;
+    private String neighborhood;
+    private String city;
+    private String state;
+    private String region;
+    private String cep;
+    private String complement;
 }
