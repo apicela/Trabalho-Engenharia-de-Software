43754e65e471b636ac06899eebbad7810d559fa3
apicela <trab.jamilsouza@gmail.com>
2024-09-06 11:10:42 -0300

    first commit

---
+++ src/main/java/com/apicela/engsoft/EngsoftApplication.java
@@ -0,0 +1,13 @@
+package com.apicela.engsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngsoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apicela/engsoft/EngsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apicela.engsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
